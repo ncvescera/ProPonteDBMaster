5976a1884b9505c1d1995b8bde9359fa8f98af77
Nicolo Vescera <nicolo.vescera@gmail.com>
2017-02-18 11:34:25 +0000

    Aggiunte pagine sociM.html e sociF.html

--- js/ajax.js
+++ js/ajax.js
@@ -13,10 +13,21 @@ function countM(){
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-     $('#M').html(this.responseText);
+     $("[id=M]").html(this.responseText);
     }
   };
   xhttp.open("GET", "./php/countM.php", true);
+  xhttp.send();
+}
+
+function loadM(){
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     $('#table').html(this.responseText);
+    }
+  };
+  xhttp.open("GET", "./php/getSociM.php", true);
   xhttp.send();
 }
 
@@ -24,9 +35,20 @@ function countF(){
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-     $('#F').html(this.responseText);
+     $("[id=F]").html(this.responseText);
     }
   };
   xhttp.open("GET", "./php/countF.php", true);
+  xhttp.send();
+}
+
+function loadF(){
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     $('#table').html(this.responseText);
+    }
+  };
+  xhttp.open("GET", "./php/getSociF.php", true);
   xhttp.send();
 }
---
+++ php/getSociF.php
@@ -0,0 +1,14 @@
+<?php
+  include 'connect.php';
+
+  $query = "SELECT id, nome, cognome FROM `Archivio Soci` WHERE sesso='F' ORDER BY id asc";
+
+  echo "<table class=\"table\">";
+  echo "<tr><th>ID</th><th>Nome</th><th>Cognome</th></tr>";
+  foreach($connessione->query($query) as $row){
+    echo "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td></tr>";
+  }
+  echo "</table>";
+
+  $connessione = null;
+?>
